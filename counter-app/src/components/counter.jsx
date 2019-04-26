3f0b75ecc196867184b8bac92d95014ba4d71573
mahfuz11 <mahfuzur.rahman@bs-23.net>
2019-04-27 04:07:00 +0600

    Updating the state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -22,8 +22,13 @@ class Counter extends Component{
     //   this.handleIncrement = this.handleIncrement.bind(this);
     // };
 
-    handleIncrement = () =>{
-      console.log("Increment Clicked", this);
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({count: this.state.count + 1})
+    };
+
+    doHandleIncrement = () => {
+        this.handleIncrement({id: 1});
     };
 
     render() {
@@ -32,7 +37,7 @@ class Counter extends Component{
             <img src={this.state.imageUrl} alt=''></img>
             <span style={this.style} className={this.getBadgeClassess()}>{this.formatCount()}</span>
             <button
-                onClick={ this.handleIncrement}
+                onClick={() => this.handleIncrement({id:1})}
                 className="btn btn-secondary btn-sm">Increment
             </button>
 
