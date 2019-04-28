0528b124139e3487bf1ac8486f06c385f1526f49
mahfuz11 <mahfuzur.rahman@bs-23.net>
2019-04-29 00:26:07 +0600

    Passing value

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 
 class Counter extends Component{
     state = {
-        count: 0,
+        value: this.props.value,
         tags : ["tag1", "tag2", "tag3"]
         // imageUrl: 'https:/picsum.photos/200'
     };
@@ -13,7 +13,7 @@ class Counter extends Component{
     };
 
     renderTags(){
-        if (this.state.count === 0) return "There is no tags !"
+        if (this.state.value === 0) return "There is no tags !"
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     };
 
@@ -24,7 +24,7 @@ class Counter extends Component{
 
     handleIncrement = product => {
         console.log(product);
-        this.setState({count: this.state.count + 1})
+        this.setState({value: this.state.value + 1})
     };
 
     doHandleIncrement = () => {
@@ -32,6 +32,7 @@ class Counter extends Component{
     };
 
     render() {
+        console.log("props", this.props)
         return (
         <div>
             <img src={this.state.imageUrl} alt=''></img>
@@ -48,13 +49,13 @@ class Counter extends Component{
 
     getBadgeClassess() {
         let classes = "badge m-2 badge-"
-        classes += (this.state.count === 0) ? "warning" : "primary"
+        classes += (this.state.value === 0) ? "warning" : "primary"
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
 
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,14 +2,19 @@ import React, { Component} from "react";
 import Counter from './counter';
 
 class Counters extends Component{
-    state  = {};
+    state  = {
+        counters: [
+            { id: 1, value:1 },
+            { id: 2, value:2 },
+            { id: 3, value:3 },
+            { id: 4, value:0 },
+        ]
+    };
     render(){
       return (
           <div>
-              <Counter />
-              <Counter />
-              <Counter />
-              <Counter />
+              { this.state.counters.map(counter =>
+                  <Counter key={counter.id} value={counter.value}/>)};
           </div>
       )
     };
