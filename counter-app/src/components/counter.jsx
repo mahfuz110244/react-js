467c9cb3a9ab71919612594348d2e60711ce37ef
mahfuz11 <mahfuzur.rahman@bs-23.net>
2019-04-29 01:33:34 +0600

    onDelete button

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 
 class Counter extends Component{
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
         tags : ["tag1", "tag2", "tag3"]
         // imageUrl: 'https:/picsum.photos/200'
     };
@@ -37,11 +37,16 @@ class Counter extends Component{
         <div>
             {/*<img src={this.state.imageUrl} alt=''></img>*/}
             {/*{this.props.children}*/}
-            <h4>Counter #{this.props.id}</h4>
+            <h4>Counter #{this.props.counter.id}</h4>
             <span style={this.style} className={this.getBadgeClassess()}>{this.formatCount()}</span>
             <button
                 onClick={() => this.handleIncrement({id:1})}
                 className="btn btn-secondary btn-sm">Increment
+            </button>
+
+            <button
+                onClick={() => this.props.onDelete(this.props.counter.id)}
+                className="btn btn-danger btn-sm m-2">Delete
             </button>
 
         </div>
