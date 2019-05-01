8b2a0af0b741002dbdf0e55d0afe29244e48e17a
mahfuz11 <mahfuzur.rahman@bs-23.net>
2019-05-01 11:58:40 +0600

    add reset option and work from parent root

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,35 +1,36 @@
 import React, { Component} from 'react';
 
 class Counter extends Component{
-    state = {
-        value: this.props.counter.value,
-        tags : ["tag1", "tag2", "tag3"]
-        // imageUrl: 'https:/picsum.photos/200'
-    };
+    // state = {
+    //     value: this.props.counter.value,
+    //     tags : ["tag1", "tag2", "tag3"]
+    //     // imageUrl: 'https:/picsum.photos/200'
+    // };
 
     style = {
       fontSize : 20,
       fontWeight: 'bold'
     };
 
-    renderTags(){
-        if (this.state.value === 0) return "There is no tags !"
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-    };
+    // renderTags(){
+    //     if (this.state.value === 0) return "There is no tags !"
+    //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    // };
 
     // constructor(){
     //   super();
     //   this.handleIncrement = this.handleIncrement.bind(this);
     // };
 
-    handleIncrement = product => {
-        console.log(product);
-        this.setState({value: this.state.value + 1})
-    };
 
-    doHandleIncrement = () => {
-        this.handleIncrement({id: 1});
-    };
+    // handleIncrement = product => {
+    //     console.log(product);
+    //     this.setState({value: this.state.value + 1})
+    // };
+
+    // doHandleIncrement = () => {
+    //     this.handleIncrement({id: 1});
+    // };
 
     render() {
         console.log("props", this.props)
@@ -40,7 +41,7 @@ class Counter extends Component{
             <h4>Counter #{this.props.counter.id}</h4>
             <span style={this.style} className={this.getBadgeClassess()}>{this.formatCount()}</span>
             <button
-                onClick={() => this.handleIncrement({id:1})}
+                onClick={() => this.props.onIncrement(this.props.counter)}
                 className="btn btn-secondary btn-sm">Increment
             </button>
 
@@ -56,12 +57,12 @@ class Counter extends Component{
 
     getBadgeClassess() {
         let classes = "badge m-2 badge-"
-        classes += (this.state.value === 0) ? "warning" : "primary"
+        classes += (this.props.counter.value === 0) ? "warning" : "primary"
         return classes;
     }
 
     formatCount(){
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value === 0 ? "Zero" : value;
     }
 }
